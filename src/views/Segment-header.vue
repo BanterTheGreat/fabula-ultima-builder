f94bc8d71acbf9fcd861192c14eb648521644559
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-18 16:13:20 +0200

    finished tag look, still need to add divider line and add correct font

--- src/views/Segment-header.vue
+++ src/views/Segment-header.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
-import { state } from '@/singletons/character-state'
-
-type TraitKey = keyof typeof state.character.traits
-
 const props = defineProps({
-  traitName: String
+  segmentHeader: String
 })
-
-const traitName = props.traitName as TraitKey
-
-let TraitInput: string = state.character.traits[traitName]
 </script>
 
 <template>
-  <div>
-    <label>
-      {{ traitName }}:
-      <input id="trait-identity" v-model="TraitInput" />
-    </label>
+  <div class="text-white text-center font-semibold bg-fabula-green rounded uppercase inline-block w-fit px-5">
+    {{segmentHeader}}
   </div>
 </template>
+
+<style scoped>
+  .w-fit {
+     width: 'fit-content'
+  }
+</style>
