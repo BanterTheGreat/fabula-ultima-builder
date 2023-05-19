ffb2cde865da09a5a5b17e5ed5e56192810d9736
Imre Baas <imspons@gmail.com>
2023-05-19 19:45:15 +0200

    WIP - Display current Bonds

--- src/components/bond.vue
+++ src/components/bond.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import type { Character } from '@/models/character';
+import  { Emotions } from '@/models/character-data/bond';
 import type Bond from '@/models/character-data/bond';
 import { CharacterUpdater } from '@/models/character-updater';
 import { state } from '@/singletons/character-state';
+import { ref } from 'vue';
 
 const props = defineProps<{
   bond: Bond,
@@ -14,11 +16,29 @@ function updateBondName(event: any) {
   new CharacterUpdater().InProperty((c: Character) => c.bonds).WithIndex(props.index).UpdateProperty<Bond, string>("name", name);
 }
 
-console.log(props.index);
+const bondEmotions = [props.bond.admirationEmotion, props.bond.affectionEmotion, props.bond.loyaltyEmotion].filter(x => x != Emotions.None);
+const bondEmotionsRef = ref(bondEmotions);
+
+function bondHasEmotion(emotion: Emotions): boolean {
+  return bondEmotions.includes(emotion);
+}
+
+function updateEmotions(event: any) {
+  console.log(bondEmotionsRef.value);
+}
 
 </script>
 
 <template>
       Bond
       <input id="trait-identity" v-model="props.bond.name" @change="updateBondName" />
+
+      <input type="checkbox" id="checkbox" v-bind:checked="bondHasEmotion(Emotions.Admiration)" v-bind:value="Emotions.Admiration" v-model="bondEmotionsRef" @change="updateEmotions" />
+      <input type="checkbox" id="checkbox" v-bind:value="Emotions.Inferiority" v-model="bondEmotionsRef" @change="updateEmotions" />
+
+      <input type="checkbox" id="checkbox" v-bind:value="Emotions.Loyalty" v-model="bondEmotionsRef" @change="updateEmotions" />
+      <input type="checkbox" id="checkbox" v-bind:value="Emotions.Mistrust" v-model="bondEmotionsRef" @change="updateEmotions" />
+
+      <input type="checkbox" id="checkbox" v-bind:value="Emotions.Affection" v-model="bondEmotionsRef" @change="updateEmotions" />
+      <input type="checkbox" id="checkbox" v-bind:value="Emotions.Hatred" v-model="bondEmotionsRef" @change="updateEmotions" />
 </template>
