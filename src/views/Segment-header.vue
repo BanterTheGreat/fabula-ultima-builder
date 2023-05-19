82e216c1fede98686837e2bf02cb7b8582ed21c1
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-19 19:43:07 +0200

    finished header styling

--- src/views/Segment-header.vue
+++ src/views/Segment-header.vue
@@ -5,13 +5,25 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="text-white text-center font-semibold bg-fabula-green rounded uppercase inline-block w-fit px-5 font-fabula-header">
-    {{segmentHeader}}
+  <div style="width: 500px">
+    <div class="text-white text-center font-semibold bg-fabula-green rounded uppercase inline-block 
+            w-fit px-5 font-fabula-header">
+      {{ segmentHeader }}
+    </div>
+    <div class="divider"></div>
   </div>
 </template>
 
 <style scoped>
-  .w-fit {
-     width: 'fit-content'
-  }
+.w-fit {
+  width: 'fit-content'
+}
+
+.divider {
+  border-bottom: 1px solid;
+  color: #2A4A41;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: -1px;
+}
 </style>
