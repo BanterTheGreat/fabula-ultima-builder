24fd900ca0d71390ce2e23836b430cb23edd50fe
Imre Baas <imspons@gmail.com>
2023-05-20 20:24:33 +0200

    Import fix

--- src/components/attribute-display.vue
+++ src/components/attribute-display.vue
@@ -3,7 +3,7 @@ import type { Character } from '@/models/character';
 import { Attribute } from '@/models/character-data/attribute';
 import { CharacterUpdater } from '@/models/character-updater';
 import { DieSize } from '@/models/die-size';
-import { state } from '@/singletons/state.js';
+import { state } from '@/singletons/state';
 
   function onChange(event: any) {
       new CharacterUpdater()
