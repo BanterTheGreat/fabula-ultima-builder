7d462c91cd8f517ce00ab8945cb8ce09d804cc13
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-21 14:52:26 +0200

    added traits back to layout and other layout changes

--- src/views/traits/trait-info.vue
+++ src/views/traits/trait-info.vue
@@ -3,7 +3,14 @@ import TraitInput from './trait-input.vue'
 </script>
 
 <template>
-  <TraitInput traitName="identity" />
-  <TraitInput traitName="theme" />
-  <TraitInput traitName="origin" />
+  <div class="grid grid-cols-6 grid-rows-2 grid-flow-row">
+    <div class="col-span-6 grid grid-cols-6 grid-rows-1" style="width: inherit !important;">
+      <!-- colspan cannot be higher than 3 because then it breaks all layouts for some reason, will update to newest tailwind and try again -->
+      <TraitInput traitName="identity" :colspanLabel=1 :colspanInput=3 />
+    </div>
+    <div class="col-span-6 grid grid-cols-6 grid-rows-1" style="width: inherit !important;">
+      <TraitInput traitName="theme" :colspanLabel=1 :colspanInput=2 />
+      <TraitInput traitName="origin" :colspanLabel=1 :colspanInput=2 />
+    </div>
+  </div>
 </template>
