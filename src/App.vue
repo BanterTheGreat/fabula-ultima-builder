8e5d79da92d8f5fbf334ee70a477d5134b0675c9
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-21 16:51:02 +0200

    layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,51 @@
 <script setup lang="ts">
-  import { CharacterCalculator } from './models/character-calculator';
+import { CharacterCalculator } from './models/character-calculator';
 
-  import SegmentBody from './components/segment-body.vue';
+import SegmentBody from './components/segment-body.vue';
 
-  import attributesVue from './views/attribute/attributes.vue';
-  import Bonds from './views/bonds.vue';
-  import Segment from './views/segment.vue';
-  import Statistic from './views/statistics.vue';
-  import TraitInfo from './views/traits/trait-info.vue';
+import attributesVue from './views/attribute/attributes.vue';
+import Bonds from './views/bonds.vue';
+import Segment from './views/segment.vue';
+import Statistic from './views/statistics.vue';
+import TraitInfo from './views/traits/trait-info.vue';
 
 
 
-  // Initial Calculating of values.
-  new CharacterCalculator().Recalculate("statistics");
+// Initial Calculating of values.
+new CharacterCalculator().Recalculate("statistics");
 </script>
 
 <template>
-  <Segment title="Traits">
-    <SegmentBody>
-      <TraitInfo />
-    </SegmentBody>
-  </Segment>
-
-  <Segment title="Attributes">
-    <SegmentBody>
-      <attributesVue />
-    </SegmentBody>
-  </Segment>
-
-  <Segment title="Bonds">
-      <SegmentBody>
-        <Bonds />
-      </SegmentBody>
-  </Segment>
-
-  <Segment title="Battle Statistics">
-      <SegmentBody>
-        <Statistic />
-      </SegmentBody>
-  </Segment>
+  <div class=" grid grid-cols-8 grid-rows-3 grid-flow-col">
+    <div class="col-span-4 mx-1">
+      <Segment title="Traits">
+        <SegmentBody>
+          <TraitInfo />
+        </SegmentBody>
+      </Segment>
+    </div>
+    <div class="col-span-4 mx-1">
+      <Segment title="Attributes">
+        <SegmentBody>
+          <attributesVue />
+        </SegmentBody>
+      </Segment>
+    </div>
+    <div class="col-span-4 mx-1">
+      <Segment title="Bonds">
+        <SegmentBody>
+          <Bonds />
+        </SegmentBody>
+      </Segment>
+    </div>
+    <div class="col-span-4 mx-1">
+      <Segment title="Battle Statistics">
+        <SegmentBody>
+          <Statistic />
+        </SegmentBody>
+      </Segment>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
