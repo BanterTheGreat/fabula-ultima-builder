aa5d1e3137219d7f031731a930a1ad4af731e687
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-21 13:36:03 +0200

    added trait info view to app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
+  import { CharacterCalculator } from './models/character-calculator';
+
+  import SegmentBody from './components/segment-body.vue';
+
   import attributesVue from './views/attribute/attributes.vue';
   import Bonds from './views/bonds.vue';
-  import SegmentBody from './components/segment-body.vue';
   import Segment from './views/segment.vue';
   import Statistic from './views/statistics.vue';
-  import { CharacterCalculator } from './models/character-calculator';
+  import TraitInfo from './views/traits/trait-info.vue';
+
+
 
   // Initial Calculating of values.
   new CharacterCalculator().Recalculate("statistics");
 </script>
 
 <template>
+  <Segment title="Traits">
+    <SegmentBody>
+      <TraitInfo />
+    </SegmentBody>
+  </Segment>
+
   <Segment title="Attributes">
     <SegmentBody>
       <attributesVue />
