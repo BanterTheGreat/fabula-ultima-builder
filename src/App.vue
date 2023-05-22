4c256daec16b2a0a18e3e9533c97b5d046cc64cd
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-22 15:30:54 +0200

    setup the basic layout and made some placeholder segments

--- src/App.vue
+++ src/App.vue
@@ -16,35 +16,54 @@ new CharacterCalculator().Recalculate("statistics");
 </script>
 
 <template>
-  <div class=" grid grid-cols-8 grid-rows-3 grid-flow-col">
-    <div class="col-span-4 mx-1">
+  <div class=" grid grid-cols-8 grid-rows-6 grid-flow-col">
+    <!-- left side start -->
+    <div class="col-span-2 row-span-1 mx-1">
+      <Segment title="Personal info">
+        <SegmentBody>
+          Segment for personal info
+        </SegmentBody>
+      </Segment>
+    </div>
+    <div class="col-span-2 row-span-1 mx-1">
       <Segment title="Traits">
         <SegmentBody>
           <TraitInfo />
         </SegmentBody>
       </Segment>
     </div>
-    <div class="col-span-4 mx-1">
-      <Segment title="Attributes">
-        <SegmentBody>
-          <attributesVue />
-        </SegmentBody>
-      </Segment>
-    </div>
-    <div class="col-span-4 mx-1">
+    <div class="col-span-2 row-span-2 mx-1">
       <Segment title="Bonds">
         <SegmentBody>
           <Bonds />
         </SegmentBody>
       </Segment>
     </div>
-    <div class="col-span-4 mx-1">
+    <div class="col-span-2 row-span-2 mx-1">
+      <Segment title="Attributes">
+        <SegmentBody>
+          <attributesVue />
+        </SegmentBody>
+      </Segment>
+    </div>
+    <!-- left side end -->
+
+    <!-- right side start -->
+    <div class="col-span-6 row-span-2 mx-1">
       <Segment title="Battle Statistics">
         <SegmentBody>
           <Statistic />
         </SegmentBody>
       </Segment>
     </div>
+    <div class="col-span-6 row-span-4 mx-1">
+      <Segment title="Battle Statistics">
+        <SegmentBody>
+          <Statistic />
+        </SegmentBody>
+      </Segment>
+    </div>
+    <!-- right side end -->
   </div>
 </template>
 
