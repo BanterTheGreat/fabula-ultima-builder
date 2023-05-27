bd098b8c03dd080f31243a2f390fc5e5c459e0e7
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-27 23:05:40 +0200

    added skill id enum

---
+++ src/enums/skill-id.ts
@@ -0,0 +1,10 @@
+export enum SkillId {
+
+    //Arcanist
+    BindAndSummon = "skl_bindandsummon",
+    RitualArcanims = "skl_ritualarcanism",
+    ArcaneRegeneration = "skl_arcaneregeneration",
+    ArcaneCircle = "sk_arcanecircle",
+    EmergencyArcanum = "skl_emergencyarcanum"
+
+}
