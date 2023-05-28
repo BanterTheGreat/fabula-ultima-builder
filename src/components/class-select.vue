7ddbf2b07440ff3a3f7a1950241208163ad15906
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-05-28 16:46:58 +0200

    added selection method for classes

---
+++ src/components/class-select.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    index: number,
+    classList: Record<string, string>
+}>();
+
+</script>
+
+<template>
+    <div>
+        <select >
+            <option></option>
+            <option v-for="className, classId in props.classList" :value="classId">{{ className }}</option>
+        </select>
+    </div>
+</template>
---
+++ src/views/class-list.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ClassSelect from '@/components/class-select.vue';
+import ClassJson from '@/data/json/class.json';
+
+let ClassIds = Object.keys(ClassJson.Classes);
+let ClassList: Record<string, string> = {};
+
+console.log(ClassIds);
+
+for (const classId of ClassIds) {
+    ClassList[classId] = ClassJson["Classes"][classId]["Name"];
+}
+
+console.log(ClassList);
+
+</script>
+
+<template>
+  <ClassSelect :index=0 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=1 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=2 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=3 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=4 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=5 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=6 :classList=ClassList></ClassSelect>
+  <ClassSelect :index=7 :classList=ClassList></ClassSelect>
+</template>
