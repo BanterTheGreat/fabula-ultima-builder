eb6d89f3810485701fd3303f1c13d28f284b7f81
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2023-06-10 15:39:03 +0200

    styling improvements

--- src/views/modals/class-select-modal.vue
+++ src/views/modals/class-select-modal.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
-  import { ClassData } from '@/data/data';
-  import type { ClassJson } from '@/models/class-json';
+import { ClassData } from '@/data/data';
+import type { ClassJson } from '@/models/class-json';
 import { ref, type Ref } from 'vue';
-  import { VueFinalModal } from 'vue-final-modal'
+import { VueFinalModal } from 'vue-final-modal'
 import TextWithBold from '../text-with-bold.vue';
 
-  let selectedClassId: string = "";
-  let selectedClass: Ref<ClassJson | undefined> = ref(undefined);
-  
-  function buttonClick(key: string) {
-    selectedClassId = key;
-    selectedClass.value = ClassData.class[selectedClassId];
-    console.log(selectedClass)
-  }
+let selectedClassId: string = "";
+let selectedClass: Ref<ClassJson | undefined> = ref(undefined);
+
+function buttonClick(key: string) {
+  selectedClassId = key;
+  selectedClass.value = ClassData.class[selectedClassId];
+  console.log(selectedClass)
+}
+
+
 </script>
 
 <template>
-  <VueFinalModal
-    class="flex justify-center items-center"
-    content-class="flex flex-col max-w-xl mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2">
+  <VueFinalModal class="flex justify-center items-center"
+    content-class="flex p-4 pl-0 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg">
 
-    <div class="grid grid-cols-4">
-      <div id="sidebar" class="bg-white flex flex-col">
-        <button v-for="(classObject, key, value) in ClassData.class" :key="key" class="text-center py-1" @click="() => buttonClick(key)">
-          {{ classObject.name }}
-        </button>
+    <div class="flex">
+      <div id="sidebar" class="bg-white flex flex-col ml-0">
+        <div v-for="(classObject, key, value) in ClassData.class" class="w-full h-[200%] hover:fabula-modal-button">
+          <button  :key="key" class="w-full h-full text-left transition ease-in-out hover:text-center"
+            @click="() => buttonClick(key)">
+            {{ classObject.name }}
+          </button>
+        </div>
       </div>
 
-      <div class="class-description">
+      <div class="class-description flex-1 p-2">
         <div v-if="selectedClass != undefined">
-          <div v-for="skill in selectedClass.skills">
-            <div class="pt-5">
+          <div v-for="skill in selectedClass.skills" class="py-5">
+            <div>
               {{ skill.name }} {{ skill.maxInvestment }}
             </div>
 
@@ -45,15 +49,15 @@ import TextWithBold from '../text-with-bold.vue';
 </template>
 
 <style scoped>
-  #sidebar {
-    /* position: fixed; */
-    width: 10rem;
-    overflow-y: scroll;
-    height: 40rem;
-  }
+#sidebar {
+  /* position: fixed; */
+  width: 10rem;
+  overflow-y: scroll;
+  height: 40rem;
+}
 
-  .class-description {
-    width: 40rem;
-    overflow-y: scroll;
-  }
+.class-description {
+  width: 70rem;
+  overflow-y: scroll;
+}
 </style>
